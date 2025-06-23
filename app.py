edce0a9221201f6627597b184a08fe94a10c6133
jacobmontoyacrs <126210029+jacobmontoyacrs@users.noreply.github.com>
2025-06-23 14:00:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 @app.route('/addLead', methods=['POST'])
 def add_lead():
     incoming = request.get_json()
-    name = incoming.get("name")
+    nombre = incoming.get("nombre")
 
-    if not name:
-        return jsonify({"error": "Falta el campo 'name' en el cuerpo"}), 400
+    if not nombre:
+        return jsonify({"error": "Falta el campo 'nombre' en el cuerpo"}), 400
 
     # URL de tu flujo Power Automate (¡incluye el ?sig=... completo!)
     power_url = "https://c0dadb8d5cc6e1b8ac840efb29924c.06.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/e9665837831b401883665783ebf3bf79/triggers/manual/paths/invoke/?api-version=1&tenantId=tId&environmentName=c0dadb8d-5cc6-e1b8-ac84-0efb29924c06&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Nh0xYPKzEU9I9Xg5crsYdTvoIn4blyGlQew6gRvluro"
@@ -20,7 +20,7 @@ def add_lead():
         "Content-Type": "application/json"
     }
 
-    data = { "name": name }
+    data = { "nombre": nombre }
 
     response = requests.post(power_url, headers=headers, data=json.dumps(data))
 
